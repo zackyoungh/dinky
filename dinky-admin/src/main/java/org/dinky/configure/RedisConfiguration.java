a63ef373a479ed135ae1b5bf55ffd2ddc576571f
zackyoungh <zackyoungh@users.noreply.github.com>
2024-12-03 05:46:31 +0000

    Spotless Apply

--- dinky-admin/src/main/java/org/dinky/configure/RedisConfiguration.java
+++ dinky-admin/src/main/java/org/dinky/configure/RedisConfiguration.java
@@ -1,6 +1,26 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.dinky.configure;
 
 import org.dinky.configure.properties.DinkyRedisProperties;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
@@ -12,6 +32,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 import redis.embedded.RedisServer;
 
 @Configuration
@@ -19,22 +40,19 @@ import redis.embedded.RedisServer;
 public class RedisConfiguration {
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnProperty(
-            prefix = "spring.redis",
-            name = "mode",
-            havingValue = "embedded",
-            matchIfMissing = true)
+    @ConditionalOnProperty(prefix = "spring.redis", name = "mode", havingValue = "embedded", matchIfMissing = true)
     @Order(-1)
-    public RedisServer redisServer(RedisProperties redisProperties){
+    public RedisServer redisServer(RedisProperties redisProperties) {
         RedisServer redisServer = new RedisServer(redisProperties.getPort());
         redisServer.start();
-        return  redisServer;
+        return redisServer;
     }
+
     @Bean
     @Order(1)
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        //设置Redis链接工厂对象
+        // 设置Redis链接工厂对象
         template.setConnectionFactory(redisConnectionFactory);
 
         // 设置键的序列化方式为 String
