21b090044f4fab4e34e5036f69db3997d6421164
ZackYoung <zackyoungh@163.com>
2024-12-12 10:59:59 +0800

    add e2e-test log

--- e2e_test/tools/login.py
+++ e2e_test/tools/login.py
@@ -3,8 +3,21 @@ from json import JSONDecodeError
 
 import requests
 from requests import Response
+from logger import log
 
+# # 创建一个logger对象
+# log = logging.getLogger(__name__)
+# # 创建一个控制台处理器
+# console_handler = logging.StreamHandler()
+# # 将控制台处理器添加到logger对象中
+# log.addHandler(console_handler)
+#
+# # 设置控制台处理器的输出格式
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# console_handler.setFormatter(formatter)
 dinky_addr = sys.argv[1]
+
+log.info(f"The address of the current request:{dinky_addr}")
 
 
 def url(path: str):
@@ -24,14 +37,13 @@ def assertRespOk(resp: Response, api_name: str):
 
 
 def login(session: requests.Session):
+    log.info("Login to Dinky, Currently in use: admin")
     login_resp: Response = session.post(url("api/login"),
                                         json={"username": "admin", "password": "dinky123!@#", "ldapLogin": False,
                                               "autoLogin": True})
     assertRespOk(login_resp, "Login")
 
+    log.info("Select the default tenant")
     choose_tenant_resp = session.post(url("api/chooseTenant?tenantId=1"))
     assertRespOk(choose_tenant_resp, "Choose Tenant")
     session.cookies.set("tenantId", '1')
-
-
-
--- e2e_test/tools/task.py
+++ e2e_test/tools/task.py
@@ -1,6 +1,9 @@
+from time import sleep
+
 import requests
 
 from login import assertRespOk, url
+from logger import log
 
 
 class CatalogueTree:
@@ -9,6 +12,12 @@ class CatalogueTree:
         self.name = name
         self.taskId = taskId
         self.children: list[CatalogueTree] = children
+
+
+def assertFlinkTaskIsRunning(status: str, name: str):
+    # todo 这里应该判断flink是否有抛出异常，而不是只有状态
+    if status != "RUNNING":
+        raise Exception(f"Flink name:{name} is not RUNNING,current status:{status}")
 
 
 def getTask(data_list: list[dict], name: str) -> CatalogueTree:
@@ -27,17 +36,28 @@ def addCatalogue(session: requests.Session, name: str, isLeaf: bool = False, par
         "isLeaf": isLeaf,
         "parentId": parentId
     })
-    assertRespOk(add_parent_dir_resp, "创建主测试路径")
+    assertRespOk(add_parent_dir_resp, "Create a dir")
     get_all_tasks_resp = session.post(url("api/catalogue/getCatalogueTreeData"), json={
         "sortValue": "",
         "sortType": ""
     })
-    assertRespOk(get_all_tasks_resp, "获取作业详情")
+    assertRespOk(get_all_tasks_resp, "Get job details")
     data_list: list[dict] = get_all_tasks_resp.json()['data']
     return getTask(data_list, name)
 
 
-def addTask(session: requests.Session, name: str, parentId: int = 0, type: str = "FlinkSql", statement: str = ""):
+def addTask(session: requests.Session, name: str, parent_id: int = 0, type: str = "FlinkSql",
+            statement: str = "") -> CatalogueTree:
+    """
+    en: Add a task
+    zh: 添加一个任务
+    :param session: requests.Session
+    :param name:  task name
+    :param parent_id:  dir id
+    :param type:  task type
+    :param statement:  statement
+    :return CatalogueTree
+    """
     add_parent_dir_resp = session.put(url("api/catalogue/saveOrUpdateCatalogueAndTask"), json={
         "name": name,
         "type": type,
@@ -54,18 +74,58 @@ def addTask(session: requests.Session, name: str, parentId: int = 0, type: str =
             "firstLevelOwner": 1
         },
         "isLeaf": False,
-        "parentId": parentId
+        "parentId": parent_id
     })
-    assertRespOk(add_parent_dir_resp, "创建作业")
+    assertRespOk(add_parent_dir_resp, "Create a task")
     get_all_tasks_resp = session.post(url("api/catalogue/getCatalogueTreeData"), json={
         "sortValue": "",
         "sortType": ""
     })
-    assertRespOk(get_all_tasks_resp, "获取作业详情")
+    assertRespOk(get_all_tasks_resp, "Get job details")
     data_list: list[dict] = get_all_tasks_resp.json()['data']
     return getTask(data_list, name)
 
 
-def runTask(session: requests.Session, taskId: int):
+def runTask(session: requests.Session, taskId: int) -> int:
+    """
+    en:Run a task
+    zh:运行一个任务
+    :param session: requests.Session
+    :param taskId: task id
+    :return:
+    """
     run_task_resp = session.get(url(f"api/task/submitTask?id={taskId}"))
-    assertRespOk(run_task_resp, "运行Task")
+    assertRespOk(run_task_resp, "Run Task")
+    return run_task_resp.json()['data']['jobInstanceId']
+
+
+def getFlinkTaskStatus(session: requests.Session, jobInstanceId: int) -> str:
+    """
+    en:  Obtain the status of a Flink task
+    zh:  获取Flink 任务状态
+    :param session:  requests.Session
+    :param jobInstanceId: job instance id
+    :return: status
+    """
+    run_task_resp = session.get(url(f"api/jobInstance/refreshJobInfoDetail?id={jobInstanceId}&isForce=false"))
+    assertRespOk(run_task_resp, "Get Task Status")
+    return run_task_resp.json()['data']['instance']['status']
+
+
+def runFlinkLocalTask(session: requests.Session, parentId: int, name: str, statement: str, waitTime: int = 10) -> None:
+    """
+    en: Run a FlinkLocal task
+    zh: 运行一个 FlinkLocal任务
+    :param session:  requests.Session
+    :param parentId:  dir id
+    :param name:  task name
+    :param statement:  statement
+    :param waitTime:  zh:等待时间
+    """
+    log.info(
+        f"======================\nA Local Flink task is currently executed，name: {name}, statement: \n{statement}\n ======================")
+    task = addTask(session, name, parentId, "FlinkSql", statement)
+    jobInstanceId = runTask(session, task.taskId)
+    sleep(waitTime)
+    status = getFlinkTaskStatus(session, jobInstanceId)
+    assertFlinkTaskIsRunning(status, name)
